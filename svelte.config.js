bf966c947be7f1b683ff3a47a6828a45312d3183
Steven Almeroth <fedor@primesite.dev>
2023-12-07 11:04:17 -0500

    nix the edge

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: 'edge',
-		})
+		adapter: adapter(),
 	},
 	preprocess: vitePreprocess(),
 	// vitePlugin: {
