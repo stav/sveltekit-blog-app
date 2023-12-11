83665ff55f4f93378f0044573ea71789405f2046
Steven Almeroth <fedor@primesite.dev>
2023-12-11 14:25:09 -0500

    export
    https://csv.js.org/stringify/options/

---
+++ src/routes/admin/export/+page.server.js
@@ -0,0 +1,36 @@
+import { Client, e } from "$lib/server/database.js"
+import { stringify } from 'csv/sync'
+
+/** @returns LoadedClientData */
+export async function load({ locals }) {
+  /**
+   * @param {ClientModel} client
+   */
+  function clientSelection (client) {
+    return {
+      first_name: true,
+      last_name: true,
+      company_name: true,
+      street: true,
+      city: true,
+      state: true,
+      zip: true,
+      email: true,
+      phone: true,
+      status: true,
+      user: { email: true, first_name: true, last_name: true, password_hash: true },
+      jobs: true,
+      order_by: client.full_name,
+      filter: e.op(client.user.email, '=', locals.user.email),
+    }
+  }
+
+  const clients = await Client.select(clientSelection)
+
+  return {
+    clients: {
+      length: clients.length,
+      csv: stringify(clients, { header: true })
+    },
+  }
+}
---
+++ src/routes/admin/export/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from 'svelte'
+  import { Button, Heading } from 'flowbite-svelte'
+  import { DownloadOutline } from 'flowbite-svelte-icons'
+
+  export let data
+
+  let blob
+
+  /** @type {string} */
+  let url
+
+	onMount(() => {
+    blob = new Blob([data.clients.csv], { type: 'text/csv;charset=utf-8' })
+    url = URL.createObjectURL(blob)
+	});
+</script>
+
+<Heading tag="h3" class="mb-8"> Export </Heading>
+
+<a href="{url}" download="export.csv">
+  <Button>
+    <DownloadOutline class="w-3.5 h-3.5 me-2" />
+    {data.clients.length}
+    Clients
+  </Button>
+</a>
